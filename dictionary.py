6cf0f2cdf4d1e6910657c67e6b35e0cea0cc52c3
phanisanam <39786744+phanisanam@users.noreply.github.com>
2019-05-15 21:11:52 +0530

    Create dictionary.py

---
+++ dictionary.py
@@ -0,0 +1,25 @@
+import json
+from difflib import get_close_matches
+accessdata=json.load(open("dictonary.json"));
+print("enter the word you want to search:");
+def getdata():
+ name=input();
+ name=name.lower(); #this converts input in to lowercase...
+ if name in accessdata:
+  res=accessdata[name];
+  for i in res:
+   print(i)
+ elif len(get_close_matches(name,accessdata.keys())) > 0:
+   print("did you mean?:",get_close_matches(name,accessdata.keys())[0]);
+   c=get_close_matches(name,accessdata.keys())[0]
+   print("say yes or NO");
+   choice=input();
+   if choice=="yes":
+    res=accessdata[c];
+   for i in res:
+    print(i)
+   else:
+    print("WORD NOT FOUND...");
+ else:
+  print("the word doesn't exist... Please double check it...");
+getdata();
